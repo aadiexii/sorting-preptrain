ab42c290e19de3c7332e2aa114d32c85d8170439
Shivam Sharma <shivam14052004@gmail.com>
2025-01-21 00:45:29 +0530

    Added New Cpp Code

--- bubble-sort/bubbleSort.cpp
+++ bubble-sort/bubbleSort.cpp
@@ -1,38 +1,31 @@
-#include <iostream>
+//C++ Program
+//Bubble Sort
+#include<bits/stdc++.h>
 using namespace std;
-
-// Bubble Sort function
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+int finalArr(vector<int> &arr){
+    int n = arr.size();
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+}
+void BubbleSort(vector <int> &arr){
+	int n= arr.size();
+    for(int i= n-1; i>=0; i--){ 
+        for(int j=0; j<=i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
             }
         }
     }
+    cout<< "Sorting is completed, lets move to printing" << endl;;
+    finalArr(arr);
 }
-
-// Main function
-int main() {
-    int arr[] = {5, 7, 8, 222, 4};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    cout << "Original array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
     }
-    cout << endl;
-
-    bubbleSort(arr, n); // Sorting the array
-
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
+    BubbleSort(arr);
 }
